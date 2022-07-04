7a8a8d5422480f6f1feaff7d7dbf31a1e74e329c
Oliver Chen <o.chen1@share.epsb.ca>
2022-07-04 17:44:08 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,47 @@
+import RPi.GPIO as GPIO
+import itertools
+
+GPIO.setmode(GPIO.BCM)
+
+CAMERA_SERVO_PIN = 18
+GPIO.setup(CAMERA_SERVO_PIN, GPIO.OUT)
+camera_servo = GPIO.PWM(CAMERA_SERVO_PIN, 50)
+
+PINS = [[17, 27, 23, 24], [5, 6, 26, 16]]
+GPIO.setup(list(itertools.chain(*PINS)), GPIO.OUT)
+motors = [[GPIO.PWM(pins, 100) for pins in motor] for motor in PINS]
+for motor in motors:
+    for pin in motor:
+        pin.start(0)
+
+
+def reset():
+    # GPIO.output(list(itertools.chain(*PINS)), GPIO.LOW)
+    for motor in itertools.chain(*motors):
+        # motor.stop()
+        motor.ChangeDutyCycle(0)
+    
+
+
+def move(direction, speed):
+    DIRECTIONS = {
+        "forward": [(0, 1), (0, 3), (1, 1), (1, 3)],
+        "backward": [(0, 0), (0, 2), (1, 0), (1, 2)],
+        "left": [(0, 1), (0, 3), (1, 0), (1, 2)],
+        "right": [(0, 0), (0, 2), (1, 1), (1, 3)],
+        "stop": []
+    }
+
+    assert direction in DIRECTIONS, "Invalid direction"
+    assert 0 <= speed <= 100, "Invalid speed"
+
+    reset()
+
+    for mid, motor in enumerate(motors):
+        for pid, pin in enumerate(motor):
+            if (mid, pid) in DIRECTIONS[direction]:
+                # pin.start(speed)
+                pin.ChangeDutyCycle(speed)
+            else:
+                # pin.stop()
+                pin.ChangeDutyCycle(0)
